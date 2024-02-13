be8bd04e12fc35dc46792d1708c64d6522c23093
Feder1711 <federicojavier.pordomingogalle@usp.ceu.esgit config --global user.name Feder1711git config --global user.email federicojavier.pordomingogalle@usp.ceu.es>
2024-02-13 09:33:49 +0100

    Primer actualiz

---
+++ src/mates/Matematicas.java
@@ -0,0 +1,31 @@
+package mates;
+
+import java.util.Random;
+
+public class Matematicas {
+    /**
+     * Genera una aproximación al número Pi mediante el método de
+     * Montecarlo. El parámetro `pasos` indica el número de puntos
+     * generado.
+     */
+    public static double generarNumeroPiIterativo(long pasos) {
+        Random rand = new Random();
+        long dentroCirculo = 0;
+
+        for (long i = 0; i < pasos; i++) {
+            double x = rand.nextDouble(); // Coordenada x aleatoria entre 0 y 1
+            double y = rand.nextDouble(); // Coordenada y aleatoria entre 0 y 1
+
+            // Calculamos la distancia al origen (0,0)
+            double distancia = Math.sqrt(x * x + y * y);
+
+            // Si la distancia es menor o igual a 1, el punto está dentro del círculo
+            if (distancia <= 1) {
+                dentroCirculo++;
+            }
+        }
+
+        // Aproximación al número pi
+        return 4.0 * dentroCirculo / pasos;
+    }
+}
